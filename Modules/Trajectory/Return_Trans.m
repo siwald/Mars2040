e14c16dd38085298e18675ef1de1560c9671bc76
AeroSigma <ericdward@gmail.com>
2015-04-16 18:14:19 -0400

    more

--- Modules/Trajectory/Return_Trans.m
+++ Modules/Trajectory/Return_Trans.m
@@ -65,11 +65,13 @@ end
 
 %% Fuel Depot Section
 for i=1:2
-    if Cur_Arch.ReturnFuel(i).Location == Location.MARS &.
+    if Cur_Arch.ReturnFuel(i).Location == Location.MARS & ...
+            strcmp(Cur_Arch.ReturnFuel(i).Name, 'O2')
         Results.Mars_ISRU.Oxidizer_Output = nansum([Results.Mars_ISRU.Oxidizer_Output, Spacecraft.Ox_Mass]); %add O2 to Mars generation
         remove_ox(Spacecraft); %remove all O2 from Spacecraft Modules
     end
-    if (ReturnFuel.MARS_LH2 == Cur_Arch.ReturnFuel(i))
+    if Cur_Arch.ReturnFuel(i).Location == Location.MARS & ...
+            strcmp(Cur_Arch.ReturnFuel(i).Name, 'LH2')
         if Cur_Arch.PropulsionType ~= Propulsion.CH4; %skip if Methane, can't gen on Mars ISRU
             Results.Mars_ISRU.Fuel_Output = nansum([Results.Mars_ISRU.Fuel_Output, Spacecraft.Fuel_Mass]); %add LH2 to Mars generation
             remove_fuel(Spacecraft); %remove all LH2 from Spacecraft Modules
